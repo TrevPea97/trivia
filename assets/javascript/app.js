4d3bee030634a9cd4e39de63a927b46787e5724b
TrevPea97 <tpeaco1003@gmail.com>
2019-04-18 18:51:23 -0400

    First commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,280 +1,149 @@
-$(document).ready(function () {
+var panel = $('#quiz-area');
 
-  // event listeners
-  $("#remaining-time").hide();
-  $("#start").on('click', trivia.startGame);
-  $(document).on('click', '.option', trivia.guessChecker);
+//button to start and submit
+$(document).on('click', '#start', function () {
 
-})
-
-var trivia = {
-  // trivia properties
-  correct: 0,
-  incorrect: 0,
-  unanswered: 0,
-  currentSet: 0,
-  timer: 20,
-  timerOn: false,
-  timerId: '',
-  // questions options and answers data
-  questions: {
-    q1: 'What is the name of Will Smiths character in Independence Day?',
-    q2: 'What year was the song My Heart Will Go On from Titanic released?',
-    q3: 'Which 90s movie featured the Looney Tunes on its soundtrack?',
-    q4: 'Which 90s movie soundtrack is the best-selling soundtrack of all time?',
-    q5: "I see dead people.",
-    q6: 'Who is the biggest selling female artist of the 90s?'
-    q7: "This 90s rapper was shot and killed in Los Angeles"
-  },
-  options: {
-    q1: ['Captain Steven Hiller', 'Blade', 'Hancock', 'Ross'],
-    q2: ['1996', '1999', '1997', '1993'],
-    q3: ['Who Framed Rodger Rabbit', 'Space Jam ', 'Looney Tunes: Back in Action ', 'Honey I Shrunk The Kids'],
-    q4: ['The Lion King ', 'Sister Act ', 'The Bodyguard', 'Grease'],
-    q5: ['Halloween', 'The Silence of The Lambs', 'The 6th Sense', 'The Butterfly Effect'],
-    q6: ['Beyonce', 'Celine Dion', 'Mariah Carey', 'Madonna'],
-    q7: ['Ice Cube', 'Biggie Smalls', 'Tupac', 'O.D.B.']
-  },
-  answers: {
-    q1: 'Captain Steven Hiller ',
-    q2: '1997',
-    q3: 'Space Jam',
-    q4: 'The Bodygaurd',
-    q5: 'The 6th Sense',
-    q6: 'Mariah Carey',
-    q7: 'Biggie Smalls'
-  },
-  // trivia methods
-  // method to initialize game
-  startGame: function () {
-    // restarting game results
-    trivia.currentSet = 0;
-    trivia.correct = 0;
-    trivia.incorrect = 0;
-    trivia.unanswered = 0;
-    clearInterval(trivia.timerId);
-
-    // show game section
-    $('#game').show();
-
-    //  empty last results
-    $('#results').html('');
-
-    // show timer
-    $('#timer').text(trivia.timer);
-
-    // remove start button
-    $('#start').hide();
-
-    $('#remaining-time').show();
-
-    // ask first question
-    trivia.nextQuestion();
-
-  },
-  // method to loop through and display questions and options 
-  nextQuestion: function () {
-
-    // set timer to 20 seconds each question
-    trivia.timer = 10;
-    $('#timer').removeClass('last-seconds');
-    $('#timer').text(trivia.timer);
-
-    // to prevent timer speed up
-    if (!trivia.timerOn) {
-      trivia.timerId = setInterval(trivia.timerRunning, 1000);
-    }
-
-    // gets all the questions then indexes the current questions
-    var questionContent = Object.values(trivia.questions)[trivia.currentSet];
-    $('#question').text(questionContent);
-
-    // an array of all the user options for the current question
-    var questionOptions = Object.values(trivia.options)[trivia.currentSet];
-
-    // creates all the trivia guess options in the html
-    $.each(questionOptions, function (index, key) {
-      $('#options').append($('<button class="option btn btn-info btn-lg">' + key + '</button>'));
-    })
-
-  },
-  // method to decrement counter and count unanswered if timer runs out
-  timerRunning: function () {
-    // if timer still has time left and there are still questions left to ask
-    if (trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length) {
-      $('#timer').text(trivia.timer);
-      trivia.timer--;
-      if (trivia.timer === 4) {
-        $('#timer').addClass('last-seconds');
-      }
-    }
-    // the time has run out and increment unanswered, run result
-    else if (trivia.timer === -1) {
-      trivia.unanswered++;
-      trivia.result = false;
-      clearInterval(trivia.timerId);
-      resultId = setTimeout(trivia.guessResult, 1000);
-      $('#results').html('<h3>Out of time! The answer was ' + Object.values(trivia.answers)[trivia.currentSet] + '</h3>');
-    }
-    // if all the questions have been shown end the game, show results
-    else if (trivia.currentSet === Object.keys(trivia.questions).length) {
-
-      // adds results of game (correct, incorrect, unanswered) to the page
-      $('#results')
-        .html('<h3>Thank you for playing!</h3>' +
-          '<p>Correct: ' + trivia.correct + '</p>' +
-          '<p>Incorrect: ' + trivia.incorrect + '</p>' +
-          '<p>Unaswered: ' + trivia.unanswered + '</p>' +
-          '<p>Please play again!</p>');
-
-      // hide game sction
-      $('#game').hide();
-
-      // show start button to begin a new game
-      $('#start').show();
-    }
-
-  },
-  // method to evaluate the option clicked
-  guessChecker: function () {
-
-    // timer ID for gameResult setTimeout
-    var resultId;
-
-    // the answer to the current question being asked
-    var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
-
-    // if the text of the option picked matches the answer of the current question, increment correct
-    if ($(this).text() === currentAnswer) {
-      // turn button green for correct
-      $(this).addClass('btn-success').removeClass('btn-info');
-
-      trivia.correct++;
-      clearInterval(trivia.timerId);
-      resultId = setTimeout(trivia.guessResult, 1000);
-      $('#results').html('<h3>Correct Answer!</h3>');
-    }
-    // else the user picked the wrong option, increment incorrect
-    else {
-      // turn button clicked red for incorrect
-      $(this).addClass('btn-danger').removeClass('btn-info');
-
-      trivia.incorrect++;
-      clearInterval(trivia.timerId);
-      resultId = setTimeout(trivia.guessResult, 1000);
-      $('#results').html('<h3>Better luck next time! ' + currentAnswer + '</h3>');
-    }
-
-  },
-  // method to remove previous question results and options
-  guessResult: function () {
-
-    // increment to next question set
-    trivia.currentSet++;
-
-    // remove the options and results
-    $('.option').remove();
-    $('#results h3').remove();
-
-    // begin next question
-    trivia.nextQuestion();
-
-  }
-
-}
-
-
-
-$(document).on("click", "#startClock", function() {
-  var timer = 50;
-  setInterval(function() {
-      timer--;
-  if (timer >= 0) {
-      span = document.getElementById("count");
-      span.innerHTML = timer;
-  }
-  if (timer === 0) {
-      alert('time is up!');
-      clearInterval(timer);
-  }
-  }, 1000);
-
-  function finish () {
-  $.each($("input[id='q1b']:checked"), function() {
-  if ($(this).val() === q1b) {
-      game.correct++;
-  }
-  else {
-      game.incorrect++;
-  }
-  });
-
-  $.each($("input[id='q2d']:checked"), function() {
-  if ($(this).val() === q2d) {
-      game.correct++;
-  }
-  else {
-      game.incorrect++;
-  }
-  });
-
-  $.each($("input[id='q3c']:checked"), function() {
-  if ($(this).val() === q3c) {
-      game.correct++;
-  }
-  else {
-      game.incorrect++;
-  }
-  });
-
-  $.each($("input[id='q4a']:checked"), function() {
-  if ($(this).val() === q4a) {
-      game.correct++;
-  }
-  else {
-      game.incorrect++;
-  }
-  });
-
-  $.each($("input[id='q5b']:checked"), function() {
-  if ($(this).val() === q5b) {
-      game.correct++;
-  }
-  else {
-      game.incorrect++;
-  }
-  });
-
-  this.result();
-
-  }
-
-  function result() {
-  clearInterval(timer);
-  $("#sresults h2").remove();
-  panel.html("<h2>All Done!</h2>");
-  panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
-  panel.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
-  panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
-  }
-
-  function finish(){
-  number = 1;
-  clearInterval(timer);
-  }
-
-  function restart(){
-  number = 50;
-  start();
-  }
-  };
-
-$(document).on("click", "#restart", function() {
-  game.restart();
+  game.start();
 });
 
-$(document).on("click", "#submit", function() {
+$(document).on('click', '#done', function () {
   game.done();
 });
 
+//Trivia Questions and Answers 
+var questions = [{
+  question: '1. What is the name of Will Smiths character in Independence Day?',
+  answers: ['Captain Steven Hiller<br>', 'Blade<br>', 'Hancock<br>', 'Ross<br>'],
+   correctAnswer: 'Captain Steven Hiller '
+}, {
+  question: '2. What year was the song My Heart Will Go On <br> from Titanic released?',
+  answers: ['1996<br>', '1999<br>', '1997<br>', '1993<br>'],
+  correctAnswer: '1997 ',
+}, {
+  question: '3. Which 90s movie featured the Looney Tunes on its soundtrack?',
+  answers: ['Who Framed Rodger Rabbit<br>', 'Space Jam <br>', 'Looney Tunes: Back in Action<br> ', 'Honey I Shrunk The Kids<br>'],
+  correctAnswer: 'Space Jam '
+}, {
+  question: '4. Which 90s movie soundtrack is <br>the best-selling soundtrack of all time?',
+  answers: ['The Lion King<br>', 'Sister Act<br>', 'The Bodyguard<br>', 'Grease<br>'],
+  correctAnswer: 'The Bodygaurd '
+}, {
+  question: "5. I see dead people.",
+  answers: ['Halloween<br>', 'The Silence of The Lambs<br>', 'The 6th Sense<br>', 'The Butterfly Effect<br>'],
+  correctAnswer: 'The 6th Sense '
+},{
+  question: '6 .Who is the biggest selling female artist of the 90s?',
+  answers: ['Beyonce<br>', 'Celine Dion<br>', 'Mariah Carey<br>', 'Madonna<br>'],
+  CorrectAnswer: 'Mariah Carey '
+}];
 
+
+//Countdown clock
+var game = {
+  correct: 0,
+  incorrect: 0,
+  counter: 45,
+
+  countdown: function () {
+    game.counter--;
+    $('#counter-number').html(game.counter);
+
+    if (game.counter === 0) {
+      console.log('TIME UP');
+      game.done();
+    }
+  },
+  start: function () {
+    timer = setInterval(game.countdown, 1000);
+
+
+    $('.container2').prepend('<h2>Time Remaining: <span id="counter-number">45</span> Seconds</h2>');
+    $('#start').remove();
+
+
+    //For loops to retreiving the info inputed by the user
+    for (var i = 0; i < questions.length; i++) {
+      panel.append('<h2>' + questions[i].question + '</h2>');
+
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        panel.append('<input type="radio" name="question' + '-' + i + '" value="' + questions[i].answers[j] + '">' + questions[i].answers[j]);
+      }
+    }
+
+    panel.append('<button id="done">SUBMIT</button>');
+  },
+  done: function () {
+
+
+    $.each($("input[name='question-0']:checked"), function () {
+      if ($(this).val() == questions[0].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-1']:checked"), function () {
+      if ($(this).val() == questions[1].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-2']:checked"), function () {
+      if ($(this).val() == questions[2].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-3']:checked"), function () {
+      if ($(this).val() == questions[3].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-4']:checked"), function () {
+      if ($(this).val() == questions[4].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-5']:checked"), function () {
+      if ($(this).val() == questions[5].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-6']:checked"), function () {
+      if ($(this).val() == questions[6].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    });
+
+    this.result();
+  },
+
+  //display the results for the user.
+  result: function () {
+
+    clearInterval(timer);
+
+    $('.container2 h2').remove();
+    panel.html('<h2>All Done!</h2>');
+    panel.append('<h3>Correct Answers: ' + this.correct + '</h3>');
+    panel.append('<h3>Incorrect Answers: ' + this.incorrect + '</h3>');
+    panel.append('<h3>Unanswered: ' + (questions.length - (this.correct + this.incorrect)) + '</h3>');
+  }
+
+};
